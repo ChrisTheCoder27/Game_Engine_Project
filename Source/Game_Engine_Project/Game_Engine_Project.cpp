2ca19faeebd907ddc481a9b15f49afaacecfcc36
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2025-09-26 12:25:50 -0400

    Initial commit

---
+++ Source/Game_Engine_Project/Game_Engine_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Game_Engine_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Game_Engine_Project, "Game_Engine_Project" );
